5d4149dd7af99f35d92d1dac2c8d1be94fb771ed
Derick Castillo <derick.castillo@daveramsey.com>
2025-05-08 06:41:23 -0500

    point to backend to properly grab data

--- src/services/blogpostService.ts
+++ src/services/blogpostService.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 export const getBlogposts = () => {
-    return axios.get('http://localhost:8080/api/v1/blogposts')
-        .then(response => { return response.data})
+    return axios.get('https://your-heroku-app-name.herokuapp.com/api/v1/blogposts')
+        .then(response => {
+            return response.data
+        })
         .catch(error => {
             console.error('Error fetching employees:', error);
             throw error;

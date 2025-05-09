33e8984634f4131681736f1761f765a3ddede53f
Derick Castillo <derick.castillo@daveramsey.com>
2025-05-09 07:47:07 -0500

    update fetch

--- src/services/blogpostService.ts
+++ src/services/blogpostService.ts
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
 export const getBlogposts = () => {
-    // return axios.get('https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts')
-    //     .then(response => {
-    //         return response.data
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching employees:', error);
-    //         throw error;
-    //     });
-    return axios.get('http://localhost:8080/api/v1/blogposts')
+    return axios.get('https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts')
         .then(response => {
             return response.data
         })
@@ -20,13 +12,7 @@ export const getBlogposts = () => {
 }
 
 export const getBlogById = (id) => {
-    // return axios.get(`https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts/${id}`)
-    //     .then(response => response.data)
-    //     .catch(error => {
-    //         console.error(`Error fetching blog with ID ${id}:`, error);
-    //         throw error;
-    //     });
-    return axios.get(`http://localhost:8080/api/v1/blogposts/${id}`)
+    return axios.get(`https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts/${id}`)
         .then(response => response.data)
         .catch(error => {
             console.error(`Error fetching blog with ID ${id}:`, error);

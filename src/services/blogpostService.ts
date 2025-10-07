0068541ac5a318630399a2b34b3f2f0ddcfa924d
Derick Castillo <derickcastillo@Dericks-MacBook-Air-2.local>
2025-10-07 15:58:38 -0500

    Update blog post service

--- src/services/blogpostService.ts
+++ src/services/blogpostService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getBlogposts = () => {
-    return axios.get('https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts')
+    return axios.get('https://blog-site-backend-8.onrender.com/api/v1/blogposts')
         .then(response => {
             return response.data
         })
@@ -12,7 +12,7 @@ export const getBlogposts = () => {
 }
 
 export const getBlogById = (id) => {
-    return axios.get(`https://dericks-blog-backend-16795d1a517d.herokuapp.com/api/v1/blogposts/${id}`)
+    return axios.get(`https://blog-site-backend-8.onrender.com/api/v1/blogposts/${id}`)
         .then(response => response.data)
         .catch(error => {
             console.error(`Error fetching blog with ID ${id}:`, error);
